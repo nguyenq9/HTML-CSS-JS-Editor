c2d1e4357a590ecf1d8fbb827016710dbabef3e4
nguyenq9 <77131873+nguyenq9@users.noreply.github.com>
2022-11-28 11:54:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+localStorage.clear();
+
+const html = document.querySelector('.html-section textarea');
+const css = document.querySelector('.css-section textarea');
+const js = document.querySelector('.js-section textarea');
+const render = document.querySelector('#render');
+
+let tag = "";
+let record = false;
+let currLen = html.value.length
+
+/*
+ * isElementSupported
+ * Feature test HTML element support 
+ * @param {String} tag
+ * @return {Boolean|Undefined}
+ */
+
+(function(win){
+    'use strict';       
+
+    var toString = {}.toString;
+
+    win.isElementSupported = function isElementSupported(tag) {
+        // Return undefined if `HTMLUnknownElement` interface
+        // doesn't exist
+        if (!win.HTMLUnknownElement) {
+            return undefined;
+        }
+        // Create a test element for the tag
+        var element = document.createElement(tag);
+        // Check for support of custom elements registered via
+        // `document.registerElement`
+        if (tag.indexOf('-') > -1) {
+            // Registered elements have their own constructor, while unregistered
+            // ones use the `HTMLElement` or `HTMLUnknownElement` (if invalid name)
+            // constructor (http://stackoverflow.com/a/28210364/1070244)
+            return (
+                element.constructor !== window.HTMLUnknownElement &&
+                element.constructor !== window.HTMLElement
+            );
+        }
+        // Obtain the element's internal [[Class]] property, if it doesn't 
+        // match the `HTMLUnknownElement` interface than it must be supported
+        return toString.call(element) !== '[object HTMLUnknownElement]';
+    };
+    
+})(this);
+
+function execute() {
+    localStorage.setItem('html', html.value);
+    localStorage.setItem('css', css.value);
+    localStorage.setItem('js', js.value);
+
+    render.contentDocument.body.innerHTML = `<style>${localStorage.css}</style>` + localStorage.html;
+    render.contentWindow.eval(localStorage.js);
+
+    if (html.value.slice(-1) == "<") {
+        record = true;
+    }
+    if (html.value.slice(-1) == ">") {
+        record = false;
+        if (tag.length != 0) {
+            let closing = "</" + tag + ">";
+            html.value += closing
+            const pos = html.value.length - closing.length;
+            // console.log(closing + " | " + tag);
+        }
+        tag = "";
+    }
+
+    if (record) {
+        if (html.value.slice(-1) != "<") {
+            if (currLen == html.value.length) {
+                tag = tag.concat(html.value.slice(-1));
+            } else {
+                let i = currLen - html.value.length;
+                tag = html.value.slice(0, -i);
+            }
+            console.log(tag);
+        }
+    }
+}
+
+html.onkeyup = () => execute();
+css.onkeyup = () => execute();
+js.onkeyup = () => execute();
+
+html.value = localStorage.html_code;
+css.value = localStorage.css_code;
+js.value = localStorage.js_code;
+
+
